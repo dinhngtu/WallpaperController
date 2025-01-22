a808fa38e88e7239f6e8e9f90606fa4cb566f56f
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2025-01-22 23:41:35 +0100

    refactor

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -1,11 +1,40 @@
 using System;
 using System.Runtime.InteropServices;
 using Windows.Win32;
+using Windows.Win32.Foundation;
 using Windows.Win32.System.SystemServices;
 
 namespace WallpaperController {
     internal static class NativeMethods {
-        public static readonly Lazy<string?> PackageContext = new(() => Utils.GetCurrentPackageFullName());
+        public static string? GetCurrentPackageFullName() {
+            // https://github.com/microsoft/Windows-AppConsult-Tools-DesktopBridgeHelpers/blob/master/DesktopBridge.Helpers/Helpers.cs
+            if (Environment.OSVersion.Version < new Version(6, 2, 0, 0)) {
+                return null;
+            }
+
+            uint length = 0;
+            WIN32_ERROR err;
+            err = PInvoke.GetCurrentPackageFullName(ref length, null);
+            if (err == WIN32_ERROR.APPMODEL_ERROR_NO_PACKAGE || err != WIN32_ERROR.ERROR_INSUFFICIENT_BUFFER) {
+                return null;
+            }
+            var buf = new char[length];
+            unsafe {
+                fixed (char* ptr = buf) {
+                    err = PInvoke.GetCurrentPackageFullName(ref length, ptr);
+                }
+            }
+            if (err == WIN32_ERROR.ERROR_SUCCESS) {
+                return new string(buf, 0, (int)length - 1);
+            } else if (err == WIN32_ERROR.APPMODEL_ERROR_NO_PACKAGE) {
+                return null;
+            } else {
+                // ignore error for now
+                return null;
+            }
+        }
+
+        public static readonly Lazy<string?> PackageContext = new(() => GetCurrentPackageFullName());
 
         public static void RevealPath(string path) {
             try {
